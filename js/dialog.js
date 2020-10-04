a578b69b09547e5a30044cf7538cc852460216fc
Olga Latkina <olga.latkina@gmail.com>
2020-10-04 22:25:47 +0300

    feat: add popup moving

--- js/dialog.js
+++ js/dialog.js
@@ -4,6 +4,7 @@
   const setup = document.querySelector(`.setup`);
   const setupOpen = document.querySelector(`.setup-open`);
   const setupClose = setup.querySelector(`.setup-close`);
+  const dialogHandle = setup.querySelector(`.upload`);
 
   const onPopupEscPress = (evt) => {
     if (evt.key === `Escape`) {
@@ -40,6 +41,55 @@
     if (evt.key === `Enter`) {
       closePopup();
     }
+  });
+
+  dialogHandle.addEventListener(`mousedown`, (evt) => {
+    evt.preventDefault();
+
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    let dragged = false;
+
+    const onMouseMove = (moveEvt) => {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      let shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = `${setup.offsetTop - shift.y}px`;
+      setup.style.left = `${setup.offsetLeft - shift.x}px`;
+    };
+
+    const onMouseUp = (upEvt) => {
+      upEvt.preventDefault();
+
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+
+      if (dragged) {
+        const onClickPreventDefault = (clickEvt) => {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener(`click`, onClickPreventDefault);
+        };
+
+        dialogHandle.addEventListener(`click`, onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
   });
 
   window.dialog = {
